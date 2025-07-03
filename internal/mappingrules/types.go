f12d1191163588cec530b81109cc1cd27871c576
Anna Rose Wiggins <annabunches@gmail.com>
2025-07-03 17:10:56 -0400

    Formatting.

--- internal/mappingrules/types.go
+++ internal/mappingrules/types.go
@@ -34,7 +34,7 @@ type LatchedMappingRule struct {
 
 type RuleTarget struct {
 	DeviceName string
-	ModeSelect	  []string
+	ModeSelect []string
 	Device     *evdev.InputDevice
 	Type       evdev.EvType
 	Code       evdev.EvCode
